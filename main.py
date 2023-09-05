b0d1813ba896f6eea3055507d909e02a00323622
Kokonico <25968788-Kokonico@users.noreply.replit.com>
2023-09-05 23:21:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+"""A module full of functions for encoding and decoding messages from classical ciphers.
+Ciphers found at en.wikipedia.org/wiki/Category:Classical_ciphers. https://inventwithpython.com/cracking/"""
+
+ALPHABET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 ,.?!'
+n = 0 # for offset cypher
+
+def adfgvx():
+  """The ADFGVX cipher. Informations at en.wikipedia.org/wiki/ADFGVX_cipher."""
+
+def offset_cipher():
+  """an offset cypher I made -Koko"""
+  cipher_text = input("enter top secret info: ")
+  offset_number = int(input("select offset key (must be number): "))
+  output = ""
+  for x in cipher_text:
+    n = 0
+    while x != ALPHABET[n]:
+      n += 1
+    n = str(n + offset_number)
+    output = output + n + "/"
+    n = int(n)
+  print(output)
+offset_cipher()

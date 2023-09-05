0079b41c26290ff6c158db2184b4379a3a008876
Kokonico <96935064+Kokonico@users.noreply.github.com>
2023-09-05 23:31:27 +0000

    added more characters to ALPHABET & code commented stuff.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 """A module full of functions for encoding and decoding messages from classical ciphers.
 Ciphers found at en.wikipedia.org/wiki/Category:Classical_ciphers. https://inventwithpython.com/cracking/"""
 
-ALPHABET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 ,.?!'
+#git enabled. press "git" in tools -Koko
+
+#for offset cypher
+ALPHA1 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+AlPHA2 = ':;/|!@#$%^&*()_+-=}{][?><,."'
+ALPHABET = ALPHA1 + AlPHA2
 n = 0 # for offset cypher
 
+  
 def adfgvx():
   """The ADFGVX cipher. Informations at en.wikipedia.org/wiki/ADFGVX_cipher."""
+
+#offset cypher
 
 def offset_cipher():
   """an offset cypher I made -Koko"""

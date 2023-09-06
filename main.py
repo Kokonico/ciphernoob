39e211c3b20bd63c7b336fbcd6ca5632eba49762
Kokonico <96935064+Kokonico@users.noreply.github.com>
2023-09-06 01:24:49 +0000

    added decryption for offset

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ Ciphers found at en.wikipedia.org/wiki/Category:Classical_ciphers. https://inven
 #git enabled. press "git" in tools -Koko
 
 #for offset cypher
-ALPHA1 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-AlPHA2 = ':;/|!@#$%^&*()_+-=}{][?><,." '
+ALPHA1 = '''abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'''
+AlPHA2 = ''':;/|!@#$%^&*()_+-=}{][?><,.'" '''
 ALPHABET = ALPHA1 + AlPHA2
 
   
@@ -34,11 +34,30 @@ def offset_cipher():
 
 # DECRYPT AREA #
 
-# offset dycrypt
+  #offset cypher decrypt
 
-# def offset_cipher_decrypt():
-
+def offset_decrypt():
+  """decrypt the offset cipher, assuming you know the key."""
+  offset_code = input("enter what you would like to decrypt: ")
+  offset_key = int(input("enter the key to said encryption: "))
+  reverse_offset = offset_key * -1
+  inlist = offset_code.split("/")
+  offpos = reverse_offset
+  offpos %= len(inlist)
+  inlist = inlist[-offpos:] + inlist[:-offpos]
+  inlist2 = []
+  result = []
+  
+  # remove offset via key
+  for a in inlist:
+    inlist2.append(int(a) - offset_key)
+  # convert to regular characters
+  print(inlist2)
+  for y in inlist2:
+    result.append(ALPHABET[y - 2])
+  print(''.join(result))
 
 # MAIN
 
 offset_cipher()
+offset_decrypt()

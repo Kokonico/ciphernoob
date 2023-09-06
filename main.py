eca2eef92a2897c5a2e02f333f5d18944c4bb4e2
Kokonico <96935064+Kokonico@users.noreply.github.com>
2023-09-06 23:21:47 +0000

    fixed issue

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ while True:
   if request == "1":
     cipher_text = input("enter top secret info: ")
     offset_number = int(input("select offset key (must be number, preferably big.): "))
-    print(cipher.kocrypter_encrypt(cipher_text, offset_number))
+    print(cipher.kocrypter.kocrypter_encrypt(cipher_text, offset_number))
   else:
     if request == "2":
       offset_code = input("enter what you would like to decrypt: ")
       offset_key = int(input("enter the key to said encryption: "))
-      print(cipher.kocrypter_decrypt(offset_code, offset_key))
+      print(cipher.kocrypter.kocrypter_decrypt(offset_code, offset_key))
     else:
       print('''usage:
       "1": encrypt.

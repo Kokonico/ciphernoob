495f261298886c08de6d9bb093b3deabc9b83388
NandaWhite <25759505-NandaWhite@users.noreply.replit.com>
2023-09-13 14:40:11 +0000

    optimized garbage code with more garbage code.

--- ciphers/ciphers/adfgvx.py
+++ ciphers/ciphers/adfgvx.py
@@ -4,26 +4,18 @@ from ciphers.utils import ALPHABET, CHARACTERS, NUMBERS
 def adfgvx(input: str, mode: bool):
   """The ADFGVX cipher. Informations at en.wikipedia.org/wiki/ADFGVX_cipher."""
   # generate grid variables.
-  chars = ALPHABET + NUMBERS
+  gridchars = input + ALPHABET + NUMBERS
   gridx = 0
   gridy = 0
   grid = {}
-  # generate reused code
   # generate grid values.
   if mode:
-    for i in input:
+    for i in gridchars:
       if i not in grid and i not in CHARACTERS:
         grid[i] = [gridx, gridy]
         gridx += 1
         if gridx == 6:
           gridx = 0
-          gridy += 1
-    for i in chars:
-      if i not in grid and i not in CHARACTERS:
-        gridx += 1
-        if gridx == 6:
-          gridx = 0
-          gridy += 1
-        grid[i] = [gridx, gridy] # behold, garbage code.
+          gridy += 1 # behold, less garbage code.
     print(grid) #TODO: remove this.
 # add else here

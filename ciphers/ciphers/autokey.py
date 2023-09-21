8a4beff7261637115b1a2d65c66902967078a412
Kokonico <96935064+Kokonico@users.noreply.github.com>
2023-09-21 01:50:17 +0000

    added spacing support

--- ciphers/ciphers/autokey.py
+++ ciphers/ciphers/autokey.py
@@ -5,7 +5,7 @@ from ciphers import utils
 
 def autokey(cmd: str, keyword: str, mode: bool, alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
     key = keyword.upper() + "".join(cmd.split()).upper()
-    cmdup = cmd.upper()
+    cmdup = cmd.upper().split()
     # create grid
     grid = {}
     current_alpha = alphabet
@@ -13,6 +13,8 @@ def autokey(cmd: str, keyword: str, mode: bool, alphabet="ABCDEFGHIJKLMNOPQRSTUV
         current_alpha = utils.str_rotate(alphabet, i)
         grid[i] = current_alpha
     if mode:
+      result = ""
+      for a in cmdup:
         # convert plaintext & key to numbers for coord locating
         numcmd = []
         numkey = []
@@ -21,19 +23,18 @@ def autokey(cmd: str, keyword: str, mode: bool, alphabet="ABCDEFGHIJKLMNOPQRSTUV
         # crop it to match length of cmd
 
         numkey = numkey[: len(cmd)]
-        for i in cmdup:
+        for i in a:
             numcmd.append(int(utils.get_index(alphabet, i)))
 
         # create final ciphertext using coords
 
-        result = ""
         tres = ""
         for index, i in enumerate(numcmd):
             tres += grid[i]
             result += tres[index]
-
-        # return result
-        return result
+        result += " "
+      # return result
+      return result
     else:
         # TODO: decryption
         return "decrypt"
